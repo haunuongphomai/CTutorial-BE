e8910a08667ccce0be6d572697082ed87a225ed4
Duong Huynh Hao (EBS.RTM) <haodh1@fpt.com>
2023-09-21 23:59:01 +0700

    Login Register

---
+++ CTutorial-BE/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CTutorial_BE.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CTutorial_BE.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Users> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>().ToTable("users");
+        }
+    }
+}
---
+++ CTutorial-BE/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using CTutorial_BE.Context;
+using CTutorial_BE.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CTutorial_BE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        public UsersController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] Users users)
+        {
+            if (users == null)
+
+                return BadRequest();
+            var user = await _appDbContext.Users.FirstOrDefaultAsync(x =>
+            x.Email == users.Email && x.Password == users.Password);
+            if (user == null)
+
+                return NotFound(new { Message = "User not found!" });
+
+
+            return Ok(new
+            {
+                Message = "Login successfully!"
+            });
+
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] Users users)
+        {
+            if (users == null)
+                return BadRequest();
+            await _appDbContext.Users.AddAsync(users);
+            await _appDbContext.SaveChangesAsync();
+            return Ok(new { Message = "Signed successfully" });
+        }
+    }
+}
